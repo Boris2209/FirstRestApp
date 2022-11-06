8a070a6985828311168aa1b5d4214604cf34a745
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-06 16:10:20 +0300

    Initial commit rest api app

---
+++ src/main/java/ru/boris/spring/FirstRestApp/models/Person.java
@@ -0,0 +1,25 @@
+package ru.boris.spring.FirstRestApp.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Person")
+@Data
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+}
